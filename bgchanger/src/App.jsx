42fc33405393831b6ea8f578cd463a928bce4396
aliabbas827 <muhammadaliabbas827@gmail.com>
2024-02-24 13:56:57 +0500

    additional colors

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -12,7 +12,12 @@ function App() {
               <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "red"}} onClick={()=> setColor("red")}>Red</button>
               <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "green"}} onClick={()=> setColor("green")}>Green</button>
               <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "blue"}} onClick={()=> setColor("blue")}>Blue</button>
-              <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "grey"}} onClick={()=> setColor("grey")}>Grey</button>
+              <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "gray"}} onClick={()=> setColor("gray")}>Gray</button>
+              <button className="outline-none px-4 py-1 rounded-full text-black shadow-lg" style={{backgroundColor: "yellow"}} onClick={()=> setColor("yellow")}>Yellow</button>
+              <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "pink"}} onClick={()=> setColor("pink")}>Pink</button>
+              <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "purple"}} onClick={()=> setColor("purple")}>Purple</button>
+              <button className="outline-none px-4 py-1 rounded-full text-black shadow-lg" style={{backgroundColor: "lavender"}} onClick={()=> setColor("lavender")}>Lavender</button>
+              <button className="outline-none px-4 py-1 rounded-full text-black shadow-lg" style={{backgroundColor: "white"}} onClick={()=> setColor("white")}>White</button>
               <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "black"}} onClick={()=> setColor("black")}>Black</button>
              </div>
          </div>
